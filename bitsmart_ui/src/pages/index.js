02438cdf80b90d750897034b73b4c93ab61fe44e
Alan Park <28246519+axpk@users.noreply.github.com>
2024-05-18 12:07:26 -0700

    Changed route connection to public url

--- bitsmart_ui/src/pages/index.js
+++ bitsmart_ui/src/pages/index.js
@@ -20,7 +20,7 @@ export default function Home() {
     if (date) {
       console.log(date);
       try {
-        const response = await fetch("http://localhost:8000/predict", {
+        const response = await fetch("https://bitsmart-backend-2b0b46a4c9ee.herokuapp.com/predict", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
